3e0a87c30eb92941a3a7eef22c4cbca0fe2446c2
Amarbilegt12 <99112462+Amarbilegt12@users.noreply.github.com>
2023-01-15 13:25:04 +0800

    Add files via upload

--- Bair.py
+++ Bair.py
@@ -2,9 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 import codecs
-from csv import writer
 
-
+rooms = str(input('Enter room qty: '))
 main_list = []
 value = []
 page_number_list = 0
@@ -12,33 +11,42 @@ main_value = []
 header = ['Шал:', 'Тагт:', 'Гараж:', 'Цонх:', 'Хаалга:', 'Цонхны тоо:', 'Барилгын явц', 'Ашиглалтанд орсон он:', 'Барилгын давхар:', 'Талбай:', 'Хэдэн давхарт:', 'Лизингээр авах боломж:', 'Дүүрэг:', 'Байршил:', 'Үнэ', 'Өрөөний Тоо']
 csv_list = []
 
-for i in range(1, 6):
-    url = f'https://www.unegui.mn/l-hdlh/l-hdlh-zarna/oron-suuts-zarna/{str(i)}-r/ulan-bator/'
-    url_uruu = requests.get(url)
-    soup = BeautifulSoup(url_uruu.content, 'html.parser')
-    page_number = soup.find('ul', class_='number-list').text.rstrip('\n').replace('\n', '')
-    page_number_list = int(page_number[-2:])
-    href_list = [a['href'] for a in soup.find_all('a', href=True)]
+url = f'https://www.unegui.mn/l-hdlh/l-hdlh-zarna/oron-suuts-zarna/{str(rooms)}-r/ulan-bator/'
+url_uruu = requests.get(url)
+soup = BeautifulSoup(url_uruu.content, 'html.parser')
+page_number = soup.find('ul', class_='number-list').text.rstrip('\n').replace('\n', '')
+page_number_list += int(page_number[-2:])
+if int(rooms) >= 5:
+    page_number_list = str(page_number_list)[1]
+    page_number_list = int(page_number_list)
+print(type(page_number_list))
+
+href_list = [a['href'] for a in soup.find_all('a', href=True)]
+href_list_1 = [x.replace('/', '') for x in href_list]
+
+for d in href_list_1:
+    if len(d) == 0:
+        href_list_1.remove(d)
+new_list = [i[3:] for i in href_list_1 if (i[0:3] == 'adv' and '_' in i)]
+main_list = main_list + new_list
+
+
+for x in range(2, page_number_list+1):
+    page_url = f'https://www.unegui.mn/l-hdlh/l-hdlh-zarna/oron-suuts-zarna/{rooms}-r/ulan-bator/?page={x}'
+    url_page = requests.get(page_url)
+    soup_pages = BeautifulSoup(url_page.content, 'html.parser')
+    href_list = [a['href'] for a in soup_pages.find_all('a', href=True)]
     href_list_1 = [x.replace('/', '') for x in href_list]
-    for d in href_list_1:
-        if len(d) == 0:
-            href_list_1.remove(d)
-    new_list = [i[3:] for i in href_list_1 if (i[0:3] == 'adv') and '_' in i]
+    for i in href_list_1:
+        if len(i) == 0:
+            href_list_1.remove(i)
+    new_list = [i[3:] for i in href_list_1 if (i[0:3] == 'adv' and '_' in i)]
     main_list = main_list + new_list
-    for x in range(2, page_number_list+1):
-        page_url = f'https://www.unegui.mn/l-hdlh/l-hdlh-zarna/oron-suuts-zarna/ulan-bator/?page={x}'
-        url_page = requests.get(page_url)
-        soup_pages = BeautifulSoup(url_page.content, 'html.parser')
-        href_list = [a['href'] for a in soup_pages.find_all('a', href=True)]
-        href_list_1 = [x.replace('/', '') for x in href_list]
-        for i in href_list_1:
-            if (len(i) == 0):
-                href_list_1.remove(i)
-        new_list = [i[3:] for i in href_list_1 if (i[0:3] == 'adv' and '_' in i)]
-        main_list = main_list + new_list
+main_list = list(set(main_list))
 
 for i in main_list:
     html_text = requests.get(f'https://www.unegui.mn/adv/{i}/')
+    print('d')
     soup = BeautifulSoup(html_text.content, 'html.parser')
     soup_all = soup.find_all('div', class_='announcement-characteristics clearfix')
     result_1 = soup.find_all('span', class_='value-chars')
@@ -54,11 +62,7 @@ for i in main_list:
     csv_list.append(sub_dic)
 print(csv_list)
 
-with codecs.open('UNEGUI.csv', 'w', 'utf-8-sig') as f:
-        writer = csv.DictWriter(f, fieldnames=header)
-        writer.writeheader()
-        writer.writerows(csv_list)
-
-
-
-
+with codecs.open(f'UNEGUI-{rooms} uruu.csv', 'w', 'utf-8-sig') as f:
+    writer = csv.DictWriter(f, fieldnames=header)
+    writer.writeheader()
+    writer.writerows(csv_list)
